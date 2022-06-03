7d135331f5cf5f57a297e84922ad71bda1119af8
OptimicPrime <96891601+OptimicPrime@users.noreply.github.com>
2022-06-03 13:52:18 -0600

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className="footer">
-	<a href='https://optimicprime.github.io/mitchelldennis/https://optimicprime.github.io/mitchelldennis/' className='footer-logo'>MD</a>
+	<a href='https://optimicprime.github.io/mitchelldennis/' className='footer-logo'>MD</a>
 	<p className="footer-text"> Designed and Built by Mitchell Dennis</p>
     </div>
   )
